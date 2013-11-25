87869b93b679dafc8fe53eeee973d90f842ba0a8
Gary Tsai <gary.m.tsai@gmail.com>
2013-11-25 01:27:49 -0800

    complete implementions of items with error messages

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -2,14 +2,14 @@ var Item = {
 	init: function() {
 		$('#new_item').on('ajax:success', this.appendItem)
 		console.log('here')
-		$('#new_item').on('ajax:done', this.throwError)
+		$('#new_item').on('ajax:error', this.throwError)
 	},
 
 	appendItem: function(event, data, status, xhr){
 		$('#error_message').empty()
 		$('#item_list').append(data.item_list)
 		var form = document.getElementById('new_item')
-		form.reset()
+			form.reset()
 	},
 
 	throwError: function(event, xhr, status, error) {
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ActiveRecord::Base
   attr_accessible :name, :price
   belongs_to :menus
-
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, uniqueness: true
 end
